50a56b4428558d23cfa9a283b3bb24b9abb2e86b
nayemk1999 <nayemk900@gmail.com>
2021-02-02 00:45:10 +0600

    True and False Check.

---
+++ trueVsFalse.js
@@ -0,0 +1,16 @@
+//false
+// 0
+// ""
+// NaN
+// null
+// false
+
+// truthy
+// "0" , true, " "
+const name = null;
+if(name){
+    console.log("Condition Is True.");
+}
+else{
+    console.log("Condition Is False.");
+}
